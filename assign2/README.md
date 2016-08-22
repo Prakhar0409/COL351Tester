468a21fc0e2e1a9845b9896a0cf44dff83cbe7a5
Prakhar0409 <prakhar0409@gmail.com>
2016-08-23 01:27:37 +0530

    added a checker for assignment 2

---
+++ assign2/README.md
@@ -0,0 +1,11 @@
+##Steps to test:
+
+1. Copy the input to input.txt
+
+2. Run your program and obtain results in output.txt
+
+3. Run the script. It calculates the row sums and the column sums to check if it matches with the input specs. Outputs hurray when all test cases passed. Shows which rows or cols dont sum upto the correct value otherwise.
+
+##LICENSE
+MIT
+
---
+++ assign2/test.py
@@ -0,0 +1,33 @@
+
+inp = [line.rstrip('\n') for line in open('input.txt')]
+outp = [line.rstrip('\n') for line in open('output.txt')]
+
+
+rows = inp[1].split(",")   
+cols = inp[2].split(",")
+
+flag = True
+vals = [[x for x in r.split(",")] for r in outp[1:]] 
+
+tmp =0
+for i in range(0,int(inp[0])):
+	tmp = 0
+	for j in range(0,int(inp[0])):
+		tmp = tmp + int(vals[i][j])
+	if(tmp!=int(rows[i])):
+		flag = False
+		print "you fucked up in %dth row sum. Expected: %d You get: %d" % (i,int(rows[i]),tmp)
+
+
+
+tmp =0
+for i in range(0,int(inp[0])):
+	tmp = 0
+	for j in range(0,int(inp[0])):
+		tmp = tmp + int(vals[j][i])
+	if(tmp!=int(cols[i])):
+		flag = False
+		print "you fucked up in %dth col sum. Expected: %d You get: %d" % (i,int(cols[i]),tmp)
+
+if flag == True:
+	print "Hurray u passed the test case"
